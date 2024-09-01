e359a51ae2ba1c19aaecf03e0219bdd0c889c761
robnsiov <robnsiov@gmail.com>
2024-09-01 19:06:43 +0330

    feature: add seo meta-tags

--- app/(pages)/(main-layout)/documentation/get/page.tsx
+++ app/(pages)/(main-layout)/documentation/get/page.tsx
@@ -1,4 +1,13 @@
 import GetDocumentation from "@/components/pages/- documentation/get";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "GET Method Documentation - Cute JSON",
+  description:
+    "Explore the documentation for the GET method in Cute JSON. Learn how to retrieve data from your APIs effectively.",
+  keywords:
+    "GET method, Cute JSON, API documentation, retrieve data, API methods",
+};
 
 const Page = () => {
   return (
@@ -8,31 +17,3 @@ const Page = () => {
   );
 };
 export default Page;
-
-const x = {
-  users: [
-    {
-      age: 22,
-      name: "john",
-      email: "john@example.com",
-      comments: [1, 2],
-      address: { country: "USA" },
-    },
-    {
-      age: 30,
-      name: "sara",
-      email: "sara@example.com",
-      comments: [3],
-      address: { country: "UK" },
-    },
-  ],
-  commnets: [
-    { id: 1, content: "this is a comment from sara" },
-    { id: 2, content: "this is another comment from sara" },
-    { id: 3, content: "this a comment from john" },
-  ],
-  prices: [30, 50, 6000, 80, 15.3, 12],
-  sizes: ["sm", "md", "lg", "xl", "2xl"],
-  verification: false,
-  hasColor: null,
-};
--- app/(pages)/(main-layout)/documentation/page.tsx
+++ app/(pages)/(main-layout)/documentation/page.tsx
@@ -1,4 +1,13 @@
 import Documentation from "@/components/pages/- documentation";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Get Started with Cute JSON",
+  description:
+    "Begin using Cute JSON with our comprehensive guide. Learn how to generate APIs, create fake data, and use the online JSON editor.",
+  keywords:
+    "Get Started, Cute JSON guide, API generation, JSON editor tutorial, fake data creation",
+};
 
 const Page = () => {
   return (
